3b7396ab4c96d9ce1568479575f7adef3741438b
levelfour <levelfourslv@gmail.com>
2015-06-15 23:12:22 +0900

    fix #10 script (implementation of auto encoder)

--- 10/digit.py
+++ 10/digit.py
@@ -35,7 +35,13 @@ class Perceptron:
             Y[i, y[i]] = 1
         return Y
 
-    def fit(self, x, y):
+    def pretrain(self, x):
+        # 事前学習
+        ae = AutoEncoder(n_dim=self.mid_dim, eta=self.eta*0.01)
+        ae.fit(x)
+        self.w = ae.weight()
+
+    def fit(self, x, y, pretraining=False):
         # 確率的勾配降下法(SGD)で学習する
         data = zip(x, y)
         np.random.shuffle(data) # データをランダムにシャッフルする
@@ -47,8 +53,15 @@ class Perceptron:
 
         self.in_dim  = X.shape[1]     # 入力ベクトルの次元数
         self.out_dim = Y.shape[1]     # 出力ベクトルの次元数
+
         self.w = np.random.uniform(-4*np.sqrt(6./(self.in_dim+self.mid_dim)), 4*np.sqrt(6./(self.in_dim+self.mid_dim)), (self.mid_dim, self.in_dim))
         self.v = np.random.uniform(-4*np.sqrt(6./(self.mid_dim+self.out_dim)), 4*np.sqrt(6./(self.mid_dim+self.out_dim)), (self.out_dim, self.mid_dim))
+
+        # オートエンコーダによる事前学習
+        # 第1層の重み初期値を推定する
+        if pretraining:
+            self.pretrain(x)
+
         for i in range(X.shape[0]):
             # 学習データを1つずつ学習する
             self.update(X[i], Y[i])
@@ -83,21 +96,20 @@ class AutoEncoder(Perceptron):
         # 中間層出力を得る
         return self.s(np.dot(x, self.w.T))
 
+    def weight(self):
+        # 重みベクトルを返す
+        return self.w
+
 if __name__ == "__main__":
     data = datasets.load_digits()
     data_x = data.data
     data_y = data.target
 
-    # AutoEncoderで学習データを次元削減する
-    ae = AutoEncoder(n_dim=16, eta=0.03)
-    ae.fit(data_x)
-    data_x = ae.reduce(data_x)
-
     train_x, test_x, train_y, test_y = cross_validation.train_test_split(data_x, data_y, test_size=0.2)
 
     # Perceptronで教師データを学習する
-    nn = Perceptron(beta=0.35)
-    nn.fit(train_x, train_y)
+    nn = Perceptron(nb_mnodes=16)
+    nn.fit(train_x, train_y, pretraining=True)
 
     # 推定
     pred_y = [nn.predict(x) for x in test_x]
